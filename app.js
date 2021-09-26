dfe4b1550dbfd7bfc1cb606ccb70f05d839fff75
Thein Than Hein <thein.than.hein.th@gmail.com>
2021-09-25 21:16:47 -0700

    Fix mapbox not showing on iphone

--- app.js
+++ app.js
@@ -61,8 +61,7 @@ const styleSrcUrls = [
 ];
 const connectSrcUrls = [
   "https://api.mapbox.com/",
-  "https://a.tiles.mapbox.com/",
-  "https://b.tiles.mapbox.com/",
+  "https://*.tiles.mapbox.com/",
   "https://events.mapbox.com/",
 ];
 const fontSrcUrls = [];
@@ -74,6 +73,7 @@ app.use(
       scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
       styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
       workerSrc: ["'self'", "blob:"],
+      childSrc: ["blob:"],
       objectSrc: [],
       imgSrc: [
         "'self'",
